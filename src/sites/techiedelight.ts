d2696e9c8238cbe02b2894cc6e457db594b563a4
Nonchalantcode <hectorg269s@outlook.com>
2021-02-12 15:10:55 -0600

    Updated techiedelight.ts

--- src/sites/techiedelight.ts
+++ src/sites/techiedelight.ts
@@ -1,31 +1,35 @@
 (function(){
-	const list = document.querySelector('#tablist1-panel1 ol')!
-	const easyProblems: HTMLLIElement[] = []
-	const mediumProblems: HTMLLIElement[] = []
-	const hardProblems: HTMLLIElement[] = []
+	try {
+		document
+			.querySelectorAll('.responsive-tabs__panel')
+			.forEach(tab => {
+				const listContainer = tab.querySelector('ol')!
+				const easy: HTMLLIElement[] = []
+				const medium: HTMLLIElement[] = []
+				const hard: HTMLLIElement[] = []
+				listContainer
+					.querySelectorAll('li')
+					.forEach(problem => {
+						const difficulty = problem.querySelector('span')!.textContent!.trim()
+						switch(difficulty) {
+							case 'Easy':
+								easy.push(problem)
+								break;
+							case 'Medium': 
+								medium.push(problem)
+								break
+							case 'Hard':
+								hard.push(problem)
+								break;
+							default:
+								console.log('No case for this', problem)
+						}
+						listContainer.removeChild(problem)
+					});
+				[...easy, ...medium, ...hard].forEach(p => listContainer.appendChild(p))
+			})
+	} catch (ex) {
+		console.log(ex.message)
+	}
 
-	list
-		.querySelectorAll('li')
-		.forEach(problem => {
-			const difficulty = problem.querySelector('span')!.textContent!.trim()
-			switch(difficulty){
-			case 'Easy': 
-				easyProblems.push(problem)
-				break
-			case 'Medium':
-				mediumProblems.push(problem)
-				break
-			case 'Hard':
-				hardProblems.push(problem)
-				break
-			default:
-				console.log('No case for this', problem)
-			}
-			
-			list.removeChild(problem)
-		});
-
-	[...easyProblems, ...mediumProblems, ...hardProblems]
-		.forEach(problem => list.appendChild(problem))
-
-})()
+}())	
